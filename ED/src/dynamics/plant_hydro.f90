jihenghu/ED2-hydro

commit 00f217637421f9a7792b88c8e184bbbc48ae2098

avoid potential floating point divide zero for cohort with few leaves in plant_hydro. Change hgt_min to 1.0 for PFT 2-4

ED/src/dynamics/plant_hydro.f90
@@ -58,6 +58,7 @@ module plant_hydro
       real                                    :: bsap       ! sapwood biomass         [kgC]
       real                                    :: crown_area ! crown area              [m2]
       real                                    :: transp     ! transpiration rate      [kg/s]
+      real                                    :: c_leaf     ! leaf capacitance        [kg/m]
       !----- Locally saved variables. -----------------------------------------------------!
       real        , save                      :: dtlsm_o_frqsum
       logical     , save                      :: first_time = .true.
@@ -151,15 +152,18 @@ module plant_hydro
                 call rwc2psi(cpatch%leaf_rwc(ico),cpatch%wood_rwc(ico),cpatch%pft(ico)     &
                             ,cpatch%leaf_psi(ico),cpatch%wood_psi(ico))
 
-                if (cpatch%bleaf(ico) > 0.) then
+                c_leaf = leaf_water_cap(cpatch%pft(ico)) * C2B * cpatch%bleaf(ico)
+                if (c_leaf > 0.) then
             
                     cpatch%leaf_psi(ico) = cpatch%leaf_psi(ico)                  &
                                          + transp * dtlsm                        &  ! kg/H2O
-                                         / ( leaf_water_cap(cpatch%pft(ico))     &
-                                           * C2B * cpatch%bleaf(ico))               ! kgH2O/m
+                                         / c_leaf                                   ! kgH2O/m
                 else
                     ! no leaves, set leaf_psi the same as wood_psi - hite
                     cpatch%leaf_psi(ico) = cpatch%wood_psi(ico) - cpatch%hite(ico)
+!                    ! need to reset rwc
+!                    call psi2rwc(cpatch%leaf_psi(ico),cpatch%wood_psi(ico),cpatch%pft(ico) &
+!                                 cpatch%leaf_rwc(ico),cpatch%wood_rwc(ico))
                 endif
                 
 
@@ -846,18 +850,21 @@ module plant_hydro
       integer   , intent(in)    ::  ipft        ! Plant functional type                [  -]
       real      , intent(out)   ::  leaf_rwc    ! Relative water content of leaves     [0-1]
       real      , intent(out)   ::  wood_rwc    ! Relative water content of wood       [0-1]
+      !----- Locals    --------------------------------------------------------------------!
+      real                      :: tot_water_sat
 
       ! leaf
-      if (bleaf > 0.) then
-          leaf_rwc          =   leaf_water_int / (leaf_water_sat(ipft) * bleaf * C2B)
+      tot_water_sat = leaf_water_sat(ipft) * C2B * bleaf
+      if (tot_water_sat > 0.) then
+          leaf_rwc          =   leaf_water_int / tot_water_sat
       else
           leaf_rwc          =   0.
       endif
       
       ! wood
-      if (broot + bdead * sap_frac > 0.) then
-          wood_rwc          =   wood_water_int / wood_water_sat(ipft)                         &
-                            /   ((broot + bdead * sap_frac) * C2B)            
+      tot_water_sat = wood_water_sat(ipft) * C2B * (broot + bdead * sap_frac) * C2B
+      if (tot_water_sat > 0.) then
+          wood_rwc          =   wood_water_int / tot_water_sat
       else
           wood_rwc          =   0.
       endif

ED/src/init/ed_params.f90
@@ -2708,7 +2708,7 @@ subroutine init_pft_alloc_params()
    !    Minimum and maximum height allowed for each cohort.                                !
    !---------------------------------------------------------------------------------------!
    hgt_min(1)     = 0.50
-   hgt_min(2:4)   = 0.50
+   hgt_min(2:4)   = 1.00
    hgt_min(5)     = 0.15
    hgt_min(6)     = 1.50
    hgt_min(7)     = 1.50

commit 37baa0777e1164bb9c4f4f7ce08867d21e1df52d

1. bug in calculating wood_psi from wood_rwc introduced from 00f2176, 2. implement limitation of SLA pheno-plasticity

ED/src/dynamics/plant_hydro.f90
@@ -139,6 +139,8 @@ module plant_hydro
                 transp      = ( cpatch%fs_open(ico) * cpatch%psi_open(ico)               &
                               + (1. - cpatch%fs_open(ico)) * cpatch%psi_closed(ico)      &
                               ) * cpatch%lai(ico) / cpatch%nplant(ico)     ! kg / s
+                !print*,'ico',ico,'DBH',cpatch%dbh(ico),'pft',cpatch%pft(ico)
+                !print*,'fs_open',cpatch%fs_open(ico),'psi_open',cpatch%psi_open(ico),'psi_closed',cpatch%psi_closed(ico),'lai',cpatch%lai(ico),'nplant',cpatch%nplant(ico)
 
 
                 ! Please notice that the current leaf_water_int has included the
@@ -863,7 +865,7 @@ module plant_hydro
       endif
       
       ! wood
-      tot_water_sat = wood_water_sat(ipft) * C2B * (broot + bdead * sap_frac) * C2B
+      tot_water_sat = wood_water_sat(ipft) * C2B * (broot + bdead * sap_frac)
       if (tot_water_sat > 0.) then
           wood_rwc          =   wood_water_int / tot_water_sat
       else

ED/src/dynamics/rk4_driver.F90
@@ -1373,7 +1373,8 @@ module rk4_driver
             !------------------------------------------------------------------------------!
          end select
          !---------------------------------------------------------------------------------!
-
+            !print*,'rk4_driver'
+            !print*,'ico',ico,'psi_open',cpatch%psi_open(ico),'psi_closed',cpatch%psi_closed(ico)
 
          !---------------------------------------------------------------------------------!
          !     Final sanity check.  This should be removed soon, since it should never     !

ED/src/dynamics/structural_growth.f90
@@ -1470,7 +1470,8 @@ subroutine update_cohort_plastic_trait(cpatch,ico)
        select case (trait_plasticity_scheme)
        case (1,2)
            ! SLA is defined at the top of canopy, use LAI to change SLA
-            cpatch%sla(ico) = SLA(ipft) * exp(ksla * max_cum_lai)
+           ! However, we only allow SLA to be doubled at the deepest shading
+            cpatch%sla(ico) = SLA(ipft) * min(2.,exp(ksla * max_cum_lai))
        case (-1,-2)
            ! SLA is defined at the bottom of canopy, use height to change SLA
             cpatch%sla(ico) = SLA(ipft) / (1. + lma_slope * cpatch%hite(ico))